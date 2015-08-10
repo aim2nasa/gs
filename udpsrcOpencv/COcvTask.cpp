9d8b5ea6027ac2f8ebc0e118550baa60686c3699
rossi <sc.kwak@funzin.co.kr>
2015-08-10 15:04:20 +0900

    320x240사이즈에 30fps로 설정하여 스트리밍 및 Object detect하는 예제 성공 라즈베리측 스크립트는 아래에..
    640x480이상에서는 화면이 많이 깨지고 Detection이 많이 느려진다.
    (그냥 스트리밍만 해서 화면을 뿌려줄때는 1080p세팅으로 해도 느려지진 않음)
    현재 컴퓨터 사양에서는 이정도가 최적으로 판단된다
    
    현재 설정에서는 gst에서 받은 스트림을 디코딩하면 yuv 4:1:1로 결과가 나오는데, 이 예제는 4에 해당하는 값만 가지고 처리한 결과임
    
    pi@raspberrypi ~/gst $ cat ul1.sh
    #!/bin/sh
    lsmod
    sudo rmmod bcm2835-v4l2
    sudo modprobe bcm2835-v4l2 gst_v4l2src_is_broken=1
    v4l2-ctl --set-fmt-video=width=320,height=240,pixelformat=4
    v4l2-ctl --set-parm=30
    v4l2-ctl --set-ctrl video_bitrate=2000000
    gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-h264,width=320,height=240,framerate=30/1 \
            ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! udpsink host=192.168.219.73 port=52156
    
    pi@raspberrypi ~/gst $

--- udpsrcOpencv/COcvTask.cpp
+++ udpsrcOpencv/COcvTask.cpp
@@ -1,5 +1,13 @@
 #include "COcvTask.h"
-#include <opencv2/opencv.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+cv::String face_cascade_name = "haarcascade_frontalface_alt.xml";
+cv::String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
+cv::CascadeClassifier face_cascade;
+cv::CascadeClassifier eyes_cascade;
+cv::string window_name = "Capture - Face detection";
 
 COcvTask::COcvTask()
 :_width(0), _height(0)
@@ -10,6 +18,9 @@ COcvTask::COcvTask()
 int COcvTask::svc(void)
 {
 	ACE_DEBUG((LM_DEBUG, "(%t) COcvTask::svc start\n"));
+	if (!face_cascade.load(face_cascade_name)) ACE_ERROR_RETURN((LM_ERROR, "%p\n", "fase cascade"), -1);
+	if (!eyes_cascade.load(eyes_cascade_name)) ACE_ERROR_RETURN((LM_ERROR, "%p\n", "eyes cascade"), -1);
+
 	ACE_Message_Block *message;
 	for (;;){
 		if (this->getq(message) == -1) ACE_ERROR_RETURN((LM_ERROR, "%p\n", "getq"), -1);
@@ -22,12 +33,45 @@ int COcvTask::svc(void)
 
 		ACE_ASSERT(_width > 0 && _height > 0);
 		cv::Mat frame(cv::Size(_width,_height), CV_8UC1, message->rd_ptr(), cv::Mat::AUTO_STEP);
-		cv::imshow("Raspberry", frame);
-		cv::waitKey(30);
+
+		detectAndDisplay(frame);
+		cv::waitKey(1);
 
 		ACE_DEBUG((LM_DEBUG, "."));
 		message->release();
 	}
 	ACE_DEBUG((LM_DEBUG, "(%t) COcvTask::svc end\n"));
 	return 0;
+}
+
+void COcvTask::detectAndDisplay(cv::Mat frame)
+{
+	std::vector<cv::Rect> faces;
+	cv::Mat frame_gray = frame;
+
+	//cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
+	cv::equalizeHist(frame_gray, frame_gray);
+	//-- Detect faces
+	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
+
+	for (size_t i = 0; i < faces.size(); i++)
+	{
+		cv::Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
+		ellipse(frame, center, cv::Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, cv::Scalar(255, 0, 255), 2, 8, 0);
+
+		cv::Mat faceROI = frame_gray(faces[i]);
+		std::vector<cv::Rect> eyes;
+
+		//-- In each face, detect eyes
+		eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
+
+		for (size_t j = 0; j < eyes.size(); j++)
+		{
+			cv::Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
+			int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
+			circle(frame, eye_center, radius, cv::Scalar(255, 0, 0), 3, 8, 0);
+		}
+	}
+	//-- Show what you got
+	cv::imshow(window_name, frame);
 }
--- udpsrcOpencv/COcvTask.h
+++ udpsrcOpencv/COcvTask.h
@@ -2,12 +2,14 @@
 #define __COCVTASK_H__
 
 #include "ace/task.h"
+#include <opencv2/opencv.hpp>
 
 class COcvTask : public ACE_Task < ACE_MT_SYNCH >
 {
 public:
 	COcvTask();
 	virtual int svc(void);
+	void detectAndDisplay(cv::Mat frame);
 
 	int _width;
 	int _height;
