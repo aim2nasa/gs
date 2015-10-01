378595caf6b5ba5d5673b5d49c174aa1eb5c429a
rossi <sc.kwak@funzin.co.kr>
2015-10-01 16:55:25 +0900

    ACE 로그 레벨 적용

--- bmpUdpsrcOpencv/main.cpp
+++ bmpUdpsrcOpencv/main.cpp
@@ -8,6 +8,7 @@
 int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
 	ACE_TRACE(ACE_TEXT("main"));
+	ACE_LOG_MSG->priority_mask(LM_INFO | LM_ERROR | LM_DEBUG, ACE_Log_Msg::PROCESS);
 
 	COcvTask ocv;
 	CGsTask gs;
