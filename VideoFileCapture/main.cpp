fa594321e6a1a0b3544e5bcf9ac524daef702f5f
Rossi Kwak <rossi.kwak@gmail.com>
2022-04-11 13:15:08 +0900

    SimpleVideoCapture를 참고하여 프로젝트 설정하고 소스도 복사해옴

--- VideoFileCapture/main.cpp
+++ VideoFileCapture/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include "opencv2/opencv.hpp"
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+	cv::VideoCapture cap(-1);
+
+	if (!cap.isOpened()) {
+		cout << "capture device not opened" << endl;
+		return -1;
+	}
+
+	cv::namedWindow("capture", 1);
+	for (;;)
+	{
+		cv::Mat frame;
+		cap >> frame;
+		cv::imshow("capture", frame);
+		if (cv::waitKey(30) >= 0) break;
+	}
 	cout << "end of VideoFileCapture" << endl;
 	return 0;
 }
